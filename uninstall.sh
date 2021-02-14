85b38c58354fe9c94795f0ecdcc650d2b1a6b193
MatthewMarmalade <marslandm@me.com>
2021-02-14 23:59:02 +0000

    Updated bash scripts

--- uninstall.sh
+++ uninstall.sh
@@ -9,15 +9,17 @@ fi
 
 # EPrelude information is located in /usr/local/share/eprelude and /usr/local/bin/ehci
 echo Deleting /usr/local/share/eprelude/Text/PrettyPrint/GenericPretty.hs...
-rm /usr/local/share/eprelude/Text/PrettyPrint/GenericPretty.hs
+rm -f /usr/local/share/eprelude/Text/PrettyPrint/GenericPretty.hs
 echo Deleting /usr/local/share/eprelude/Text/PrettyPrint...
 rmdir /usr/local/share/eprelude/Text/PrettyPrint
 echo Deleting /usr/local/share/eprelude/Text...
 rmdir /usr/local/share/eprelude/Text
-echo Deleting /usr/local/share/eprelude/*
-rm /usr/local/share/eprelude/*
+echo Deleting /usr/local/share/eprelude/.ghci
+rm -f /usr/local/share/eprelude/.ghci
+echo Deleting /usr/local/share/eprelude/EPrelude.hs
+rm -f /usr/local/share/eprelude/EPrelude.hs
 echo Deleting /usr/local/share/eprelude...
 rmdir /usr/local/share/eprelude
 echo Deleting /usr/local/bin/ehci
-rm /usr/local/bin/ehci
+rm -f /usr/local/bin/ehci
 echo Uninstall Complete.
