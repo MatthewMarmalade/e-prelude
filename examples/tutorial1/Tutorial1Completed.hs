cddc978dd19f3b859863a5f634fc89b22a134043
MatthewMarmalade <marslandm@me.com>
2021-03-22 15:06:53 +0000

    Revert "Removed extraneous tutorial file."
    This reverts commit fba69560beb16d0cb80b5bd1fa05775fd7b23292.

---
+++ examples/tutorial1/Tutorial1Completed.hs
@@ -0,0 +1,80 @@
+module Tutorial1Completed where
+
+import EdPrelude
+import PicturesSVG -- needed for the optional chess part
+import Test.QuickCheck
+
+
+-- 2.
+double :: Integer -> Integer
+double x = x + x
+
+square :: Integer -> Integer
+square x = x * x
+
+-- 3.
+isTriple :: Integer -> Integer -> Integer -> Bool
+isTriple a b c = square a + square b == square c
+
+-- 4.
+leg1 :: Integer -> Integer -> Integer
+leg1 x y = square x - square y
+
+leg2 :: Integer -> Integer -> Integer
+leg2 x y = 2 * y * x
+
+hyp :: Integer -> Integer -> Integer
+hyp x y = square x + square y
+
+-- 5.
+prop_triple :: Integer -> Integer -> Bool
+prop_triple x y = isTriple (leg1 x y) (leg2 x y) (hyp x y)
+
+-- 7.
+pic1 :: Picture
+pic1 = above (beside knight (invert knight)) (beside (invert knight) knight)
+
+pic2 :: Picture
+pic2 = above (beside knight (invert knight)) (flipV (beside knight (invert knight)))
+
+-- ** Functions
+
+twoBeside :: Picture -> Picture
+twoBeside x = beside x (invert x)
+
+-- 8.
+twoAbove :: Picture -> Picture
+twoAbove x = above x (invert x)
+
+fourPictures :: Picture -> Picture
+fourPictures x = twoAbove (twoBeside x)
+
+-- 9.
+-- a)
+emptyRow :: Picture
+emptyRow = repeatH 4 (beside whiteSquare blackSquare)
+
+-- b)
+otherEmptyRow :: Picture
+otherEmptyRow = repeatH 4 (beside blackSquare whiteSquare)
+
+-- c)
+middleBoard :: Picture
+middleBoard = above (above emptyRow otherEmptyRow) (above emptyRow otherEmptyRow)
+
+-- d)
+whiteRow :: Picture
+whiteRow = over (beside (beside (beside rook knight) (beside bishop queen)) (beside (beside king bishop) (beside knight rook))) otherEmptyRow
+
+blackRow :: Picture
+blackRow = over (invert (beside (beside (beside rook knight) (beside bishop queen)) (beside (beside king bishop) (beside knight rook)))) emptyRow
+
+-- e)
+whitePawns :: Picture
+whitePawns = over (repeatH 8 pawn) emptyRow
+
+blackPawns :: Picture
+blackPawns = over (repeatH 8 (invert pawn)) otherEmptyRow
+
+populatedBoard :: Picture
+populatedBoard = above (above blackRow blackPawns) (above middleBoard (above whitePawns whiteRow))
---
+++ experiments/Tutorial2.hs
@@ -0,0 +1,128 @@
+module Tutorial2 where
+
+import EdPrelude
+--import Data.Char
+--import Data.List
+--import Test.QuickCheck
+--import Control.Monad (guard)
+
+
+-- 1. halveEvens
+
+-- List-comprehension version
+halveEvens :: [Integer] -> [Integer]
+halveEvens xs = undefined
+
+-- This is for testing only. Do not try to understand this (yet).
+halveEvensReference :: [Integer] -> [Integer]
+halveEvensReference = (>>= \x -> guard (x `mod` 2 == 0) >>= \_ -> return $ x `div` 2)
+
+-- Mutual test
+prop_halveEvens :: [Integer] -> Bool
+prop_halveEvens xs = undefined
+
+
+-- 2. inRange
+
+-- List-comprehension version
+inRange :: Integer -> Integer -> [Integer] -> [Integer]
+inRange lo hi xs = undefined
+
+
+-- 3. countPositives
+
+-- List-comprehension version
+countPositives :: [Integer] -> Integer
+countPositives list = undefined
+
+
+-- 4. multDigits
+
+-- List-comprehension version
+multDigits :: String -> Integer
+multDigits str = undefined
+
+countDigits :: String -> Integer
+countDigits str = undefined
+
+prop_multDigits :: String -> Bool
+prop_multDigits xs = undefined
+
+
+-- 5. capitalise
+
+-- List-comprehension version
+capitalise :: String -> String
+capitalise s = undefined
+
+
+-- 6. title
+
+lowercase :: String -> String
+lowercase xs = undefined
+
+-- List-comprehension version
+title :: [String] -> [String]
+title _ = undefined
+
+
+-- 7. signs
+
+sign :: Integer -> Char
+sign i = undefined
+
+signs :: [Integer] -> String
+signs xs = undefined
+
+
+-- 8. score
+
+score :: Char -> Integer
+score x  = undefined
+
+totalScore :: String -> Integer
+totalScore xs = undefined
+
+prop_totalScore_pos :: String -> Bool
+prop_totalScore_pos xs = undefined
+
+
+-- 9. pennypincher
+
+-- List-comprehension version.
+pennypincher :: [Integer] -> Integer
+pennypincher prices = undefined
+
+-- And the test itself
+prop_pennypincher :: [Integer] -> Bool
+prop_pennypincher xs = undefined
+
+
+-- ** Optional Material
+
+-- 10. crosswordFind
+
+-- List-comprehension version
+crosswordFind :: Char -> Integer -> Integer -> [String] -> [String]
+crosswordFind letter pos len words = undefined
+
+
+-- 11. search
+
+-- List-comprehension version
+search :: String -> Char -> [Integer]
+search str goal = undefined
+
+-- Depending on the property you want to test, you might want to change the type signature
+prop_search :: String -> Char -> Bool
+prop_search str goal = undefined
+
+
+-- 12. contains
+
+contains :: String -> String -> Bool
+contains str substr = undefined
+
+-- Depending on the property you want to test, you might want to change the type signature
+prop_contains :: String -> String -> Bool
+prop_contains str1 str2 = undefined
