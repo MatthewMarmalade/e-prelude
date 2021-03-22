1f354d439d2beec4cd8b31c088fb09c552ab54c7
MatthewMarmalade <marslandm@me.com>
2021-03-22 13:56:24 +0000

    Minimized file.

--- edprelude/EdPrelude.hs
+++ edprelude/EdPrelude.hs
@@ -44,7 +44,7 @@ module EdPrelude (
     --List Types and Functions
     length, take, drop, takeWhile, dropWhile, sum, product, and, or, all, any, (!!), zip, zipWith, unzip, isPrefixOf, map, elem, (++),
     repeat, replicate, head, tail, init, last, concat, delete, maximum, minimum, reverse, filter, curry, uncurry, intersperse,
-    sort, sortOn, sortBy, nub, nubBy, unlines, lines, unwords, words, concatMap,
+    sort, sortOn, sortBy, nub, nubBy, unlines, lines, unwords, words, concatMap, transpose, (\\), genericLength,
     foldr, foldr1, foldl, foldl1,
     --Pair Functions
     fst, snd,
@@ -85,10 +85,14 @@ import Data.Eq
 import Data.Ord
 import qualified GHC.Enum as E (Enum,toEnum,fromEnum)
 import qualified Data.Char as C (ord,chr)
-import Data.List (sort,sortOn,sortBy,nubBy)
+import Data.List (sort,sortOn,sortBy,nub,nubBy,genericLength,(\\),transpose,intersperse,head,tail,init,last,
+                  repeat,map,filter,reverse,delete,takeWhile,dropWhile,zip,unzip,zipWith,isPrefixOf)
+import Data.String (lines,unlines,words,unwords)
+import Data.Char (isDigit,isAlpha,isAlphaNum,toLower,toUpper,isLower,isUpper)
+import Data.Tuple (fst,snd,curry,uncurry)
 import GHC.Show
 import GHC.Types (Char, Bool(True,False), Double)
-import GHC.Base (String, error, errorWithoutStackTrace, ($), (.), undefined, seq, sequence)
+import GHC.Base (String, error, errorWithoutStackTrace, ($), (.), undefined, seq, sequence, id, (||), (&&), otherwise, not)
 import Data.Maybe
 import Control.Monad (Monad, liftM, liftM2, (>>=), guard, return)
 import Control.Applicative (Applicative)
@@ -101,6 +105,7 @@ import System.Random (randomR, randomRIO)
 -- Num Functions
 
 -- Bool Functions
+{-
 otherwise :: Bool
 otherwise = True
 
@@ -117,14 +122,15 @@ infixr 3 &&
 (&&) :: Bool -> Bool -> Bool
 (&&) True True  = True
 (&&) _ _        = False
-
+-}
 -- Enum Functions
+
 toEnum :: E.Enum a => Integer -> a
 toEnum i = E.toEnum (fromIntegral i)
 
 fromEnum :: E.Enum a => a -> Integer
 fromEnum x = toInteger (E.fromEnum x)
-
+{-
 -- Char Functions
 isDigit :: Char -> Bool
 isDigit c = c >= '0' && c <= '9'
@@ -150,6 +156,7 @@ toLower :: Char -> Char
 toLower c
     | isUpper c = toEnum (fromEnum c - fromEnum 'A' + fromEnum 'a')
     | otherwise = c
+-}
 
 digitToInteger :: Char -> Integer
 digitToInteger c 
@@ -168,9 +175,6 @@ chr :: Integer -> Char
 chr i = toEnum i
 
 -- List Functions
-(++) :: [a] -> [a] -> [a]
-(++) [] y = y
-(++) (x:xs) y = x : (++) xs y
 
 length :: [a] -> Integer
 length [] = 0
@@ -181,25 +185,14 @@ take n [] = []
 take 0 (x:xs) = []
 take n (x:xs) = x : take (n-1) xs
 
-takeWhile :: (a -> Bool) -> [a] -> [a]
-takeWhile _ [] = []
-takeWhile f (x:xs)  | f x = x : takeWhile f xs
-                    | otherwise = []
-
 drop :: Integer -> [a] -> [a]
 drop n [] = []
 drop 0 xs = xs
 drop n (x:xs) = drop (n-1) xs
 
-dropWhile :: (a -> Bool) -> [a] -> [a]
-dropWhile _ [] = []
-dropWhile f (x:xs)  | f x = dropWhile f xs
-                    | otherwise = (x:xs)
-
-delete :: Eq a => a -> [a] -> [a]
-delete _ [] = []
-delete a (x:xs) | a == x = xs
-                | otherwise = x : delete a xs
+(++) :: [a] -> [a] -> [a]
+(++) [] y = y
+(++) (x:xs) y = x : (++) xs y
 
 sum :: Num a => [a] -> a
 sum [] = 0
@@ -233,16 +226,65 @@ minimum [] = errorWithoutStackTrace "EdPrelude.minimum: empty list"
 minimum [x] = x
 minimum (x:xs) = min x (minimum xs)
 
-reverse :: [a] -> [a]
-reverse [] = []
-reverse (x:xs) = (reverse xs) ++ [x]
-
 infixl 9 !!
 (!!) :: [a] -> Integer -> a
 xs     !! n | n < 0 =  errorWithoutStackTrace "EdPrelude.!!: negative index"
 []     !! _         =  errorWithoutStackTrace "EdPrelude.!!: index too large"
 (x:_)  !! 0         =  x
 (_:xs) !! n         =  xs !! (n-1)
+
+concatMap :: (a -> [b]) -> [a] -> [b]
+concatMap f as = concat (map f as)
+
+elem :: (Eq a) => a -> [a] -> Bool
+elem x [] = False
+elem x (y:ys) = x == y || elem x ys
+
+replicate :: Integer -> a -> [a]
+replicate n x = take n (repeat x)
+
+concat :: [[a]] -> [a]
+concat [] = []
+concat (xs:xss) = xs ++ concat xss
+
+foldr :: (a -> b -> b) -> b -> [a] -> b
+foldr _ v [] = v
+foldr f v xs = f (head xs) (foldr f v (tail xs))
+
+foldl :: (a -> b -> a) -> a -> [b] -> a
+foldl _ v [] = v
+foldl f v xs = f (foldl f v (init xs)) (last xs)
+
+foldr1 :: (a -> a -> a) -> [a] -> a
+foldr1 _ [] = errorWithoutStackTrace "EdPrelude.foldr1: empty list"
+foldr1 _ [x] = x
+foldr1 f xs = f (head xs) (foldr1 f (tail xs))
+
+foldl1 :: (a -> a -> a) -> [a] -> a
+foldl1 _ [] = errorWithoutStackTrace "EdPrelude.foldl1: empty list"
+foldl1 _ [x] = x
+foldl1 f xs = f (foldl1 f (init xs)) (last xs)
+
+{-
+
+takeWhile :: (a -> Bool) -> [a] -> [a]
+takeWhile _ [] = []
+takeWhile f (x:xs)  | f x = x : takeWhile f xs
+                    | otherwise = []
+
+dropWhile :: (a -> Bool) -> [a] -> [a]
+dropWhile _ [] = []
+dropWhile f (x:xs)  | f x = dropWhile f xs
+                    | otherwise = (x:xs)
+
+delete :: Eq a => a -> [a] -> [a]
+delete _ [] = []
+delete a (x:xs) | a == x = xs
+                | otherwise = x : delete a xs
+
+reverse :: [a] -> [a]
+reverse [] = []
+reverse (x:xs) = (reverse xs) ++ [x]
 
 map :: (a -> b) -> [a] -> [b]
 map _ [] = []
@@ -252,9 +294,6 @@ filter :: (a -> Bool) -> [a] -> [a]
 filter _ [] = []
 filter f (a:as) | f a = a : filter f as
                 | otherwise = filter f as
-
-concatMap :: (a -> [b]) -> [a] -> [b]
-concatMap f as = concat (map f as)
 
 fst :: (a,b) -> a
 fst (a,b) = a
@@ -289,10 +328,6 @@ isPrefixOf [] _ = True
 isPrefixOf _ [] = False
 isPrefixOf (x:xs) (y:ys) = x == y && isPrefixOf xs ys
 
-elem :: (Eq a) => a -> [a] -> Bool
-elem x [] = False
-elem x (y:ys) = x == y || elem x ys
-
 nub :: (Eq a) => [a] -> [a]
 nub xs = reverse (nubHelp [] xs) where
     nubHelp seen [] = seen
@@ -302,35 +337,10 @@ nub xs = reverse (nubHelp [] xs) where
 repeat :: a -> [a]
 repeat x = x : (repeat x)
 
-replicate :: Integer -> a -> [a]
-replicate n x = take n (repeat x)
-
 intersperse :: a -> [a] -> [a]
 intersperse _ [] = []
 intersperse a [x] = [x]
 intersperse a (x:xs) = x : a : intersperse a xs
-
-words :: String -> [String]
-words [] = []
-words s = (takeWhile (/= ' ') s) : words (drop 1 (dropWhile (/= ' ') s))
-
-lines :: String -> [String]
-lines [] = []
-lines s = (takeWhile (/= '\n') s) : lines (drop 1 (dropWhile (/= '\n') s))
-
-unwords :: [String] -> String
-unwords [] = ""
-unwords [x] = x
-unwords (w:ws) = w ++ [' '] ++ unwords ws
-
-unlines :: [String] -> String
-unlines [] = ""
-unlines [x] = x
-unlines (l:ls) = l ++ ['\n'] ++ unlines ls
-
-concat :: [[a]] -> [a]
-concat [] = []
-concat (xs:xss) = xs ++ concat xss
 
 head :: [a] -> a
 head [] = errorWithoutStackTrace "EdPrelude.head: empty list"
@@ -350,36 +360,12 @@ init [] = []
 init [x] = []
 init (x:xs) = x : init xs
 
-transpose :: [[a]] -> [[a]]
-transpose [] = []
-transpose xs = (concatMap (take 1) xs) : transpose (filter (\x -> length x > 0) (map (drop 1) xs))
-
-foldr :: (a -> b -> b) -> b -> [a] -> b
-foldr _ v [] = v
-foldr f v xs = f (head xs) (foldr f v (tail xs))
-
-foldl :: (a -> b -> a) -> a -> [b] -> a
-foldl _ v [] = v
-foldl f v xs = f (foldl f v (init xs)) (last xs)
-
-foldr1 :: (a -> a -> a) -> [a] -> a
-foldr1 _ [] = errorWithoutStackTrace "EdPrelude.foldr1: empty list"
-foldr1 _ [x] = x
-foldr1 f xs = f (head xs) (foldr1 f (tail xs))
-
-foldl1 :: (a -> a -> a) -> [a] -> a
-foldl1 _ [] = errorWithoutStackTrace "EdPrelude.foldl1: empty list"
-foldl1 _ [x] = x
-foldl1 f xs = f (foldl1 f (init xs)) (last xs)
+-}
 -- Undefined
 
 --Monads
 replicateM :: Applicative m => Integer -> m a -> m [a]
 replicateM i m = M.replicateM (fromIntegral i) m
-
---Miscellaneous
-id :: a -> a
-id a = a
 
 --Pretty-Printing
 
